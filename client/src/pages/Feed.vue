cc2c0ecd603a9c7f89f75e5c2c56772f63f30c06
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-07 18:47:07 -0400

    new post in feed

--- client/src/pages/Feed.vue
+++ client/src/pages/Feed.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="feed">
+    <NewPost />
     <h3>Feed</h3>
     <div v-for="post in posts" :key="post.id">
       <Post :post="post" />
@@ -10,11 +11,13 @@
 <script>
 import Post from "../components/Post.vue";
 import { GetPosts } from "../services/PostServices";
+import NewPost from '../components/NewPost.vue'
 
 export default {
   name: "Feed",
   components: {
     Post,
+    NewPost
   },
   data: () => ({
     posts: [],
