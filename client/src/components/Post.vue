7db095874bf863995d00f2e9b8e96bc0a198819a
jinimbrancalhao <jinjinim717@gmail.comgit config --global user.email jinjinim717@gmail.com>
2021-10-07 17:33:45 -0400

    some routes tested

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="post">
-  <h3>Post</h3>
+    <img :src="post.costume" alt="">
+    <h4>{{post.content}}</h4>
+    <h2>{{post.claps}}</h2>
+  
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: 'Post',
+  props: [
+    'post'
+  ]
 }
 </script>
--- client/src/pages/Feed.vue
+++ client/src/pages/Feed.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="feed">
     <h3>Feed</h3>
+    <div v-for="post in posts" :key="post.id">
+      <Post :post="post"/>
+    </div>
   </div>
 </template>
 
 <script>
+import Post from '../components/Post.vue'
+import {GetPosts} from '../services/PostServices'
+
 export default {
   name: "Feed",
-  components: {}
-}
+  components: {
+    Post
+  },
+  data: () => ({
+    posts: []
+  }),
+  mounted: function(){
+    this.getPosts()
+  },
+  methods: {
+    async getPosts(){
+    this.posts = await GetPosts()
+    console.log(this.posts)
+  }
+}}
 </script>
-
-<style>
-
-</style>
