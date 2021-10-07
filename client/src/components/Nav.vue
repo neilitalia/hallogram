79f421a296c5b756c27d6cc6b7634e44ede11d5e
Neil Italia <38524171+neilitalia@users.noreply.github.com>
2021-10-07 15:24:00 -0500

    create new post component

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -2,11 +2,12 @@
   <nav>
     <router-link to="/">Feed</router-link>
     <router-link to="/profile">Profile</router-link>
+    <router-link to="/newpost">New Post</router-link>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'Nav'
-}
+  name: "Nav",
+};
 </script>
---
+++ client/src/components/NewPost.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Create a new post</h1>
+    <form action="">
+      <input type="text" placeholder="costume" />
+      <textarea
+        v-model="content"
+        name="content"
+        cols="30"
+        rows="10"
+        placeholder="content"
+        maxlength="256"
+      />
+      <p>{{ 256 - content.length }}/256</p>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewPost",
+  data: () => ({
+    content: "",
+    costume: null,
+    user_id: 0,
+  }),
+  methods: {
+    sendPost() {
+      if (this.content && this.costume && this.user_id) {
+        const payload = {
+          content: this.content,
+          costume: this.costume,
+          claps: 0,
+          user_id: this.user_id,
+        };
+        console.log(`payload :>>`, payload);
+        // TODO: Add new post service here
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
